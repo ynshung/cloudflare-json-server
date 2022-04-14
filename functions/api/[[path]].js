223b727ade1bcab5a9d2e4f2e0aafe56b39a8252
Young Shung <me@ynshung.com>
2022-04-14 16:04:39 +0800

    remove domain checking

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,23 +1,8 @@
 import db from "../../db.json";
 
-var allowed = ['cloudflare-json-server.pages.dev'];
-
 export async function onRequestGet({ request }) {
     const { pathname, searchParams } = new URL(request.url);
-
-    let headers = new Headers({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-        'Access-Control-Allow-Headers': '*'
-    });
-
-    if (!allowed.includes(request.headers.get('origin'))) {
-        return new Response('Not allowed', {
-            status: 403,
-            headers: headers
-        });
-    }
-
+    
     // Split by / and remove first empty element
     var paths = pathname.split('/').slice(1);
 
@@ -39,7 +24,7 @@ export async function onRequestGet({ request }) {
     if (hideData) hideData.forEach(e => delete data[e]);
 
     if (!data) return new Response("Not found", { status: 404 });
-    
+
     return new Response(JSON.stringify(data), {
         status: 200,
         headers: headers
